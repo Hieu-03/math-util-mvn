f3399a0da28350ec01ffa6bdb51a458b3635278c
Hieu-03 <hieupnse171321@fpt.edu.vn>
2023-06-28 11:35:21 +0700

    add screenshots and update source using recursion to see green forever

--- src/main/java/org/hieu/mathutil/core/MathUtility.java
+++ src/main/java/org/hieu/mathutil/core/MathUtility.java
@@ -9,18 +9,34 @@ package org.hieu.mathutil.core;
  * @author Hieu-Acer
  */
 public class MathUtility {
+//    public static long getFactorial(int n){
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz.");
+//        if (n == 0 || n == 1)
+//            return 1;
+//        
+//        long product = 1; //tích nhân dồn 1 2 3 4 5...
+//        for (int i = 2; i <= n; i++)
+//            product *= i;
+//        
+//        return product;
+//    }
     public static long getFactorial(int n){
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz.");
         if (n == 0 || n == 1)
             return 1;
-        
-        long product = 1; //tích nhân dồn 1 2 3 4 5...
-        for (int i = 2; i <= n; i++)
-            product *= i;
-        
-        return product;
+        return n * getFactorial(n - 1);
     }
+    
+    //5! = 1.2.3.4  .5 
+    //   = 4!       .5
+    //4! = 3!   .4
+    //3! = 2! .3
+    //2! = 1!.2
+    //1! = 1
+    
+    //N! = N . (N-1)!
 }
 
 //TDD: testing driven development
